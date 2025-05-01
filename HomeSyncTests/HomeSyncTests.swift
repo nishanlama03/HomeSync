6c08e9d10509c34ffe865c66f34c5a46b7a2e9ee
nishan <nishanlama03@gmail.com>
2025-04-30 21:13:21 -0700

    Initial Commit

---
+++ HomeSyncTests/HomeSyncTests.swift
@@ -0,0 +1,17 @@
+//
+//  HomeSyncTests.swift
+//  HomeSyncTests
+//
+//  Created by Nishan Lama on 4/30/25.
+//
+
+import Testing
+@testable import HomeSync
+
+struct HomeSyncTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
